0b6dba4d0810ee565fa0348e6addcf2b78c9051c
Sagar Hande <sagar.hande.work@gmail.com>
2023-01-03 15:08:28 +0530

    added custom ModelAdmin class for custom User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Account
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    # The fields to be used in displaying the User model in Admin panel.
+    list_display = (
+        'email', 'first_name', 'last_name', 'username', 'phone_number', 'last_login', 'date_joined',)
+    # The fields need to be clickable
+    list_display_links = ('email', 'first_name', 'last_name', 'username')
+    # Readonly
+    readonly_fields = ('last_login', 'date_joined')
+    # Ordering
+    ordering = ('-date_joined',)
+
+    # Default
+    list_filter = ()
+    filter_horizontal = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
