d8b79b83ba27ed0e89f2c2b82db1bedd926e408b
Sagar Hande <sagar.hande.work@gmail.com>
2023-01-04 11:59:40 +0530

    changes to show products of specific category

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,5 @@ from . import views
 
 urlpatterns = [
         path('', views.store, name='store'),
+        path('<slug:category_slug>/', views.store, name='products_by_category'),
     ]
--- store/views.py
+++ store/views.py
@@ -1,15 +1,29 @@
 # Standard library imports.
 
 # Django imports.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # First party imports.
 from category.models import Category
+from .models import Product
 
 
-def store(request):
+def store(request, category_slug=None):
     categories = Category.objects.all()
+    category = None
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, is_available=True)
+    else:
+        products = Product.objects.all().filter(is_available=True)
+
+    products_count = products.count()
+
     context = {
+        'category': category,
         'categories': categories,
+        'products': products,
+        'products_count': products_count,
         }
     return render(request, 'store/store.html', context=context)
