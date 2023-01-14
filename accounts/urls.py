5471b81749910f5137bc9673b930fc6fff14de80
Sagar Hande <sagar.hande.work@gmail.com>
2023-01-14 19:15:06 +0530

    changes for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
       path("register/", views.register, name="register"),
       path("login/", views.login, name="login"),
       path("logout/", views.logout, name="logout"),
+      path("dashboard/", views.dashboard, name="dashboard"),
+      path("", views.dashboard, name="dashboard"),
+
       path("activate/<uidb64>/<token>/", views.activate, name="activate"),
-       path("dashboard/", views.dashboard, name="dashboard"),
+      path("forgot-password/", views.forgot_password, name="forgot-password"),
+      path("reset-password-validate/<uidb64>/<token>/", views.reset_password_validate, name="reset-password-validate"),
+      path("reset-password/", views.reset_password, name="reset-password"),
     ]
