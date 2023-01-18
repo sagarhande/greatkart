d89d171784691ba8a943cae9707f8c88014e74fb
Sagar Hande <sagar.hande.work@gmail.com>
2023-01-18 17:04:00 +0530

    added urls and view + some UI change

--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# Django imports
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
+
+# First party imports
+from cart.models import CartItem
 
 # Create your views here.
+
+
+@login_required(login_url="login")
+def place_order(request):
+    user = request.user
+
+    cart_items = CartItem.objects.filter(user=user)
+
+    return render("")
