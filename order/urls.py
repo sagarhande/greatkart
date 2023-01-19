c18075ded1938fffb55a78b5baec3b664d73480b
Sagar Hande <sagar.hande.work@gmail.com>
2023-01-19 14:51:18 +0530

    changes for place-order page

--- order/urls.py
+++ order/urls.py
@@ -8,5 +8,6 @@ from . import views
 
 
 urlpatterns = [
-    path("place-order", views.place_order, name="place-order"),
+    path("place-order/", views.place_order, name="place-order"),
+    path("payments/", views.payments, name="payments"),
 ]
